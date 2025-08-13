9ebbb71f8b001445440d6bbb82d5c2559ba5bd58
Cyan <contact@cyans.me>
2025-08-13 19:05:40 +0800

    chore: update

--- build.rs
+++ build.rs
@@ -63,6 +63,10 @@ fn main() {
         .define("HAVE_TIME_H", None)
         .out_dir(&lib);
 
+    if cfg!(feature = "quictls") {
+        cfg.define("HAVE_QUICTLS", None);
+    }
+
     if target.contains("windows") {
         // Apparently MSVC doesn't have `ssize_t` defined as a type
         if target.contains("msvc") {
